e4b11a478fb629e3564e8d0133c76f6ad50e9119
chuckdebrum <chuck_deb@yahoo.com>
2017-12-18 16:18:23 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,75 +1,37 @@
-var backgrounds = [
-    "images/tech.jpg",
-    "images/best-tech-sites.jpg",
-    "images/healthtech.jpg",
-    "images/high-tech-wallpaper.jpg"
-];
+$(document).ready(function() {
 
-$(".side-bar > a").siblings().hide();
-$(".side-bar > a").click(function (){
+    $("#greeting").addClass("animated bounceInDown");
+    $("#job-role").addClass("animated bounceInLeft");
 
-    $(this).siblings().toggle();
-    console.log("This button is working");
+    $(".column-left, .column-right").waypoint(function() {
+        $(".column-left").addClass("animated swing");
+        // $(".column-right").addClass("animated swing");
+    },
+    {
+        offset: '50%'
+    });
 
-    $(this).toggleClass("open");
+    $(".my-skills").waypoint(function() {
+        $(".my-skills").addClass("animated fadeInUp");
+        
+    },
+    {
+        offset: '40%'
+    });
 
-    $(".back1").css("background-image", "url(" + backgrounds[0] +  ")");
-    $(".back2").css("background-image", "url(" + backgrounds[1] +  ")");
-    $(".back3").css("background-image", "url(" + backgrounds[2] +  ")");
-    $(".back4").css("background-image", "url(" + backgrounds[3] +  ")");
+    $(".my-projects").waypoint(function() {
+        $(".my-projects").addClass("animated fadeInUp");
+        
+    },
+    {
+        offset: '40%'
+    });
 
-    if($(this).hasClass("open")) {
-
-        $(".side-bar").animate({
-
-            width: "40%"
-            }, 1000, function(){
-                $(".side-bar > a").text("Close");   
-         });
-
-    } else {
-
-        $(".side-bar").animate({
-            
-            width: "12%"
-            }, 1000, function(){
-                $(".side-bar > a").text("Open");   
-         });
-    }   
-});
-
-$(".newBackgrounds > div").click(function() {
-    $(this).toggleClass("addBorder");
-
-    $(this).siblings().removeClass("addBorder");
-});
-
-
-
-$("#updateButton").click(function() {
-    
-    var newIntroText = $("#introText").val();
-    var newIntroTextColor = $("#introTextColor").val();
-
-    var newJobTitle = $("#jobTitle").val();
-    var newJobTitleColor = $("#jobTitleColor").val();
-
-    $(".hero-container > p").text(newIntroText);
-    $(".hero-container > p").css("color", newIntroTextColor);
-
-    $(".hero-container > h1").text(newJobTitle);
-    $(".hero-container > h1").css("color", newJobTitleColor);
-
-    if( $(".back1").hasClass("addBorder") ) {
-        $("header").css("background-image", "url(" + backgrounds[0] + ")");
-
-    } else if ($(".back2").hasClass("addBorder") ){
-        $("header").css("background-image", "url(" + backgrounds[1] + ")");
-
-    } else if ($(".back3").hasClass("addBorder") ){
-        $("header").css("background-image", "url(" + backgrounds[2] + ")");
-
-    } else {
-        $("header").css("background-image", "url(" + backgrounds[3] + ")");
-    }
+    $(".contact-section").waypoint(function() {
+        $(".contact-section").addClass("animated fadeInUp");
+        
+    },
+    {
+        offset: '70%'
+    });
 });
